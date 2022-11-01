d2eed83cae6f3fb079545966073aa4e170b6da83
NaamaCahen <cahenbb@gmail.com>
2022-11-01 12:12:14 +0200

    challenge 4 -labyrinth

--- gameScript.js
+++ gameScript.js
@@ -1,5 +1,5 @@
 (function displayFirstChallenge() {
-    nextChallenge(3);
+    nextChallenge(1);
 })()
 function nextChallenge(challengeNum) {
     let h1 = document.getElementById("challengeTitle");
@@ -19,6 +19,7 @@ function nextChallenge(challengeNum) {
             break;
         case 4:
             displayChallenge4(h1, h3, section, timer, answers);
+            break;
         default:
             displayChallenge1(h1, h3, section, timer, answers);
             break;
@@ -27,49 +28,53 @@ function nextChallenge(challengeNum) {
 function displayChallenge1(h1, h3, section, timer, answers) {
     h1.textContent = "Challenge #1 -Colors"
     h3.textContent = "choose the color which is colored in the same color of its name!"
-    const src="./images/colors.jpg";
-    challengeImage(section,src);
+    const src = "./images/colors.jpg";
+    challengeImage(section, src);
     answersBox(answers, 1);
     timerFunc(timer, 15);
 }
-function challengeImage(section,src) {
+
+function displayChallenge2(h1, h3, section, timer, answers) {
+    h1.textContent = "Challenge #2 -Frequency";
+    h3.textContent = "choose the most frequent element!"
+    const src = "./images/frequency.jpg";
+    challengeImage(section, src);
+    answersBox(answers, 2);
+    timerFunc(timer, 15);
+}
+function displayChallenge3(h1, h3, section, timer, answers) {
+    h1.textContent = "Challenge #3 -Puzzle";
+    h3.textContent = "choose the piece of puzzle which will complete the puzzle!"
+    const src = "./images/puzzle.jpg";
+    challengeImage(section, src);
+    answersBox(answers, 3);
+    timerFunc(timer, 15);
+}
+function displayChallenge4(h1, h3, section, timer, answers) {
+    h1.textContent = "Challenge #4 -Labyrinth";
+    h3.textContent = "were will the arrow arrive to? \n choose the correct destination!"
+    const src = "./images/labyrinth.jpg";
+    challengeImage(section, src);
+    answersBox(answers, 4);
+    timerFunc(timer, 15);
+}
+function challengeImage(section, src) {
     let img1 = document.createElement("img");
     img1.src = src;
     img1.style.width = "35%";
     img1.alt = "colors";
     section.appendChild(img1);
 }
-function displayChallenge2(h1, h3, section, timer, answers) {
-    h1.textContent = "Challenge #2 -Frequency";
-    h3.textContent="choose the most frequent element!"
-    const src="./images/frequency.jpg";
-    challengeImage(section,src);
-    answersBox(answers, 2);
-    timerFunc(timer, 15);
-}
-function displayChallenge3(h1, h3, section, timer, answers) {
-    h1.textContent = "Challenge #3 -Puzzle";
-    h3.textContent="choose the piece of puzzle which will complete the puzzle!"
-    const src="./images/puzzle.jpg";
-    challengeImage(section,src);
-    answersBox(answers, 3);
-    timerFunc(timer, 15);
-}
-function displayChallenge4(h1, h3, section, timer, answers) {
-    h1.textContent = "Challenge #4"
-
-}
-
 function timerFunc(timer, seconds) {
     let timerInterval = setInterval(() => {
         seconds--;
-        if (seconds>0){
-            document.querySelector("#timer>p").textContent=`${seconds}`;
-        }else if(seconds===0){
-            document.querySelector("#timer>p").textContent=`${seconds}`;
+        if (seconds > 0) {
+            document.querySelector("#timer>p").textContent = `${seconds}`;
+        } else if (seconds === 0) {
+            document.querySelector("#timer>p").textContent = `${seconds}`;
             clearInterval(timerInterval);
             //go to answer.html display-wrong
-            window.location.href="answer.html";
+            window.location.href = "answer.html";
         }
     }, 1000)
 
@@ -87,29 +92,35 @@ function answersBox(answersDiv, challengeNum) {
             span.addEventListener('click', answerPage);
         }
     } else if (challengeNum === 2) {
-        const ballsArray=[1,14,8,11];
-        for(let i=0;i<ballsArray.length;i++){
-            let spanContainer=document.createElement("span");
+        const ballsArray = [1, 14, 8, 11];
+        for (let i = 0; i < ballsArray.length; i++) {
+            let spanContainer = document.createElement("span");
             let spanBall = document.createElement("span");
-            spanBall.textContent ="sports_baseball";
+            spanBall.textContent = "sports_baseball";
             spanBall.classList.add("material-symbols-outlined");
-            let spanNum=document.createElement("span");
-            spanNum.textContent=`${ballsArray[i]}`;
-            spanContainer.append(spanBall,spanNum);
+            let spanNum = document.createElement("span");
+            spanNum.textContent = `${ballsArray[i]}`;
+            spanContainer.append(spanBall, spanNum);
             answersDiv.appendChild(spanContainer);
             spanContainer.addEventListener('click', answerPage);
         }
     } else if (challengeNum === 3) {
-        const optionsArr=["A ","B ","C "];
+        const optionsArr = ["A ", "B ", "C "];
         for (const option of optionsArr) {
-            let span=document.createElement("span");
-            span.textContent=`${option}`;
-            span.addEventListener("click",answerPage);
+            let span = document.createElement("span");
+            span.textContent = `${option}`;
+            span.addEventListener("click", answerPage);
             answersDiv.appendChild(span);
         }
 
     } else if (challengeNum === 4) {
-
+        const optionsArr = ["A ", "B ", "C "];
+        for (const option of optionsArr) {
+            let span = document.createElement("span");
+            span.textContent = `${option}`;
+            span.addEventListener("click", answerPage);
+            answersDiv.appendChild(span);
+        }
     }
 }
 function answerPage(event) {
