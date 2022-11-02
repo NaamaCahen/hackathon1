5bbeb9cac1d2d6a4c9a82019d37566bf48240ed5
NaamaCahen <cahenbb@gmail.com>
2022-11-02 03:26:40 +0200

    styled the result page

--- results.js
+++ results.js
@@ -11,10 +11,11 @@
 })()
 function displayWin() {
     document.querySelector("h1").textContent = "WINNER!!!";
-    document.querySelector("h3").textContent = "Your brain is sharp and quick thinking ! \n WELL DONE! ";
+    document.querySelector("h2").textContent = "Your brain is sharp and quick thinking ! \n WELL DONE! ";
 }
 function displayNotWInner(brain) {
     document.querySelector("h1").textContent = "WELL DONE,";
-    document.querySelector("h3").textContent = `You got ${brain} parts of the brain! \n  Feel free to try your luck in a replay game...`;
+    document.querySelector("h2").textContent = `You got ${brain} parts of the brain! \n  Feel free to try your luck in a replay game...`;
+    document.body.style.backgroundImage="radial-gradient(violet, blue)";
 }
 
